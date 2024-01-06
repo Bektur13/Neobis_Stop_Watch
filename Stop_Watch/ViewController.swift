4acf8b595b4cfea14cee9eac4d1b7bb98a06d5a5
Bektur13 <Bektur13@github.com>
2024-01-06 12:48:48 -0800

    Initial Commit

---
+++ Stop_Watch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stop_Watch
+//
+//  Created by Бектур Дуйшембеков on 1/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
